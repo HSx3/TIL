ad3d52f288836f889fdd5f1935728031025f61c9
HSx3 <hs.ssafy@gmail.com>
2019-04-01 09:15:12 +0900

    algorithm add

--- algorithm/add/반사경.py
+++ algorithm/add/반사경.py
@@ -12,6 +12,9 @@ def go(bx, by):
             nx = bx + dx[i]
             ny = by + dy[i]
 
+            if nx < 0 or nx >= n or ny < 0 or ny >= n:
+                return count
+
             if data[nx][ny] == 1:
                 if bx == nx and by > ny:     # 오른쪽에서 왼쪽
                     data[nx][ny] = 9
@@ -27,14 +30,14 @@ def go(bx, by):
                     dy = [0]
                     bx = nx
                     by = ny
-                if bx < nx and by == ny:     # 아래에서 위
+                if bx > nx and by == ny:     # 아래에서 위
                     data[nx][ny] = 9
                     count += 1
                     dx = [0]
                     dy = [1]
                     bx = nx
                     by = ny
-                if bx > nx and by == ny:     # 위에서 아래
+                if bx < nx and by == ny:     # 위에서 아래
                     data[nx][ny] = 9
                     count += 1
                     dx = [0]
@@ -57,32 +60,29 @@ def go(bx, by):
                     dy = [0]
                     bx = nx
                     by = ny
-                if bx < nx and by == ny:     # 아래에서 위
+                if bx > nx and by == ny:     # 아래에서 위
                     data[nx][ny] = 9
                     count += 1
                     dx = [0]
                     dy = [-1]
                     bx = nx
                     by = ny
-                if bx > nx and by == ny:     # 위에서 아래
+                if bx < nx and by == ny:     # 위에서 아래
                     data[nx][ny] = 9
                     count += 1
                     dx = [0]
                     dy = [1]
                     bx = nx
                     by = ny
-            elif data[nx][ny] == 0:
+            elif data[nx][ny] == 0 or data[nx][ny] == 9:
                 data[nx][ny] = 9
                 bx = nx
                 by = ny
-            elif nx < 0 or nx >= n or ny < 0 or ny >= n:
-                return count
+
 
 
 T = int(input())
 for test_case in range(1, T+1):
      n = int(input())
      data = [list(map(int, input().split())) for _ in range(n)]
-     # print(data)
-
-print(go(0, 0))
+     print('#{} {}'.format(test_case, go(0, 0)))
